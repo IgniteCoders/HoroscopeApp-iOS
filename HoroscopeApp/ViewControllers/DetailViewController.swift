86cef32f31550798e56e61c47e716cf518f31e64
IgniteCoders <theboom.kajas@gmail.com>
2025-06-20 17:23:55 +0200

    We added the call to the horoscope API to show the luck of the day, week or month

--- HoroscopeApp/ViewControllers/DetailViewController.swift
+++ HoroscopeApp/ViewControllers/DetailViewController.swift
@@ -13,10 +13,15 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var horoscopeNameLabel: UILabel!
     @IBOutlet weak var horoscopeDatesLabel: UILabel!
     
+    @IBOutlet weak var horoscopeLuckTextView: UITextView!
+    @IBOutlet weak var loadingView: UIActivityIndicatorView!
+    
     @IBOutlet weak var favoriteMenu: UIBarButtonItem!
     
     var horoscope: Horoscope!
     var isFavorite: Bool = false
+    
+    var horoscopeLuck: String? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,6 +36,8 @@ class DetailViewController: UIViewController {
         isFavorite = SessionManager.isFavoriteHoroscope(id: horoscope.id)
         
         setFavoriteImage()
+        
+        getHoroscopeLuck(period: "daily")
     }
     
     func setFavoriteImage() {
@@ -48,9 +55,65 @@ class DetailViewController: UIViewController {
     }
     
     @IBAction func share(_ sender: Any) {
-        
+        if let text = horoscopeLuck {
+            let textToShare = [ text ]
+            let activityViewController = UIActivityViewController(activityItems: textToShare, applicationActivities: nil)
+            
+            activityViewController.popoverPresentationController?.sourceView = self.view // so that iPads won't crash
+            
+            self.present(activityViewController, animated: true, completion: nil)
+        }
     }
     
+    @IBAction func didChangePeriod(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            getHoroscopeLuck(period: "daily")
+        case 1:
+            getHoroscopeLuck(period: "weekly")
+        default:
+            getHoroscopeLuck(period: "monthly")
+        }
+    }
+    
+    func getHoroscopeLuck(period: String) {
+        loadingView.isHidden = false
+        guard let url = URL(string: "https://horoscope-app-api.vercel.app/api/v1/get-horoscope/\(period)?sign=\(horoscope.id)&day=TODAY") else {
+            self.loadingView.isHidden = true
+            return // ERROR
+        }
+        Task {
+            do {
+                let (data, _) = try await URLSession.shared.data(from: url)
+                
+                /*if let str = String(data: data, encoding: .utf8) {
+                    print("Successfully decoded: \(str)")
+                }*/
+                
+                guard let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {
+                    DispatchQueue.main.async {
+                        self.loadingView.isHidden = true
+                    }
+                    return // ERROR
+                }
+                
+                let jsonData = json["data"] as? [String: String]
+                
+                horoscopeLuck = jsonData?["horoscope_data"] ?? ""
+                
+                DispatchQueue.main.async {
+                    self.horoscopeLuckTextView.text = self.horoscopeLuck
+                    self.loadingView.isHidden = true
+                }
+            } catch {
+                print(error.localizedDescription)
+                DispatchQueue.main.async {
+                    self.loadingView.isHidden = true
+                }
+            }
+        }
+        
+    }
     /*
     // MARK: - Navigation
 
